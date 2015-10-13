57b00e290f8dbd25338e6878910278615446cc33
badylq <badylq@outlook.com>
2015-10-13 15:46:58 +0200

    init

---
+++ Models/KategoriaProduktu.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models
+{
+	public class KategoriaProduktu
+	{
+		public int Id { get; set; }
+		public string Nazwa { get; set; }
+		public string Opis { get; set; }
+		public virtual ICollection<Produkt> Produkty { get; set; }
+	}
+}
